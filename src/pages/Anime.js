cc336fb3c03aa6d6fc0b8562c9636f25c5c0411c
arfi.justika <arfi@justika.com>
2022-07-09 17:34:25 +0700

    add simple anime detail page

--- src/pages/Anime.js
+++ src/pages/Anime.js
@@ -1,14 +1,67 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import styled from '@emotion/styled';
 
-import Header from '../components/Header';
+import Layout from '../components/Layout';
+import QueryResult from '../components/QueryResult';
+
+const StyledDescription = styled.p({
+  whiteSpace: 'pre-line'
+})
+
+export const ANIME = gql`
+  query Media($mediaId: Int) {
+    Media(id: $mediaId) {
+      id
+      title {
+        english
+        native
+        romaji
+      }
+      bannerImage
+      coverImage {
+        medium
+        large
+        extraLarge
+      }
+      description
+      episodes
+      genres
+      averageScore
+    }
+  }
+`;
 
 function Anime({ id }) {
+  const { loading, error, data } = useQuery(ANIME, {
+    variables: { mediaId: id }
+  });
+
   return (
-    <div>
-      <Header />
-      <h1>Anime Page</h1>
-      <p>{id}</p>
-    </div>
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <img
+          src={data?.Media.coverImage.extraLarge}
+          alt={data?.Media.title.romaji}
+          css={{
+            width: 'fit-content'
+          }}
+        />
+        <div>
+          <h2>{data?.Media.title.romaji}</h2>
+          <StyledDescription>{data?.Media.description}</StyledDescription>
+          <b>Episodes</b>
+          <p>{data?.Media.episodes}</p>
+          <b>Genres</b>
+          {data?.Media.genres.map(genre =>
+            <p key={genre}>{genre}</p>
+          )}
+          <b>Average Score</b>
+          <p>{data?.Media.averageScore}%</p>
+        </div>
+      </QueryResult>
+    </Layout>
   );
 }
 
